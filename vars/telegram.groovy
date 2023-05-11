278c374be3b773c70d16ec9f28a95bdc96bc8eef
Thieu Quan Ngoc <ngocthieu245@gmail.com>
2023-05-11 12:27:48 +0700

    Fix telegram.groovy

--- vars/telegram.groovy
+++ vars/telegram.groovy
@@ -1,4 +1,4 @@
-def telegramSend(def payload) {
+def send(def payload) {
     try {
         if (env.TG_BOT_TOKEN == null) {
             throw new Exception("TG_BOT_TOKEN is not set")
@@ -7,13 +7,13 @@ def telegramSend(def payload) {
         if (chatId == null) {
             throw new Exception("CHAT_ID is not set")
         }
-        if (message == null) {
+        if (payload.message == null) {
             throw new Exception("Message is not set")
         }
         String telegramApi = "https://api.telegram.org/bot${env.TG_BOT_TOKEN}/sendMessage"
-        String shCommand = "curl -X POST $telegramApi -d chat_id=$chatId -d text=$payload.message -d parse_mode='markdown'"
+        String shCommand = "curl -X POST $telegramApi -d chat_id=$chatId -d text=\"${payload.message}\" -d parse_mode='markdown'"
         sh(shCommand)
     } catch (Exception e) {
-        println("Could not send telegram message: ${e.message}")
+        println("Could not send telegram message: ${e.getMessage()}")
     }
 }
