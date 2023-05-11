6ae34ac5b7b7ea77e037a01f874ed0fc3b504368
Thieu Quan Ngoc <ngocthieu245@gmail.com>
2023-05-11 12:17:52 +0700

    Create telegram.groovy

---
+++ telegram.groovy
@@ -0,0 +1,19 @@
+def telegramSend(def payload) {
+    try {
+        if (env.TG_BOT_TOKEN == null) {
+            throw new Exception("TG_BOT_TOKEN is not set")
+        }
+        def chatId = payload.chatId != null ? payload.chatId : env.TG_NOTIFICATION_CHAT
+        if (chatId == null) {
+            throw new Exception("CHAT_ID is not set")
+        }
+        if (message == null) {
+            throw new Exception("Message is not set")
+        }
+        String telegramApi = "https://api.telegram.org/bot${env.TG_BOT_TOKEN}/sendMessage"
+        String shCommand = "curl -X POST $telegramApi -d chat_id=$chatId -d text=$payload.message -d parse_mode='markdown'"
+        sh(shCommand)
+    } catch (Exception e) {
+        println("Could not send telegram message: ${e.message}")
+    }
+}
